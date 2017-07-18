64c5586694f9af26387f0ee44ed735052a4d38a2
Gregor Reitzenstein <dean4devil@paranoidlabs.org>
2017-07-18 13:26:33 +0200

    Makes readBode actually work

--- readBode.m
+++ readBode.m
@@ -3,9 +3,11 @@ function data = readBode(fileName)
     data = strsplit(data, "\n");
     data = strjoin(data(2:numel(data)), "\n");
     data = strrep(data, "\t", ";");
-    data = strrep(data, ",", " ");
+    data = strrep(data, ",", ";");
     data = strrep(data, "°", "");
     data = strrep(data, "dB", "");
+    data = strrep(data, "(", "");
+    data = strrep(data, ")", "");
     tmp = tempname;
     f = fopen(tmp, "w+");
     fputs(f, data);
